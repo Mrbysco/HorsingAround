2e948e61463c65c97023298bb507a56adf43433d
Mrbysco <Mrbysco@users.noreply.github.com>
2023-12-20 17:02:56 +0100

    Fix server crash

--- src/main/java/com/mrbysco/horsingaround/network/message/SyncDataMessage.java
+++ src/main/java/com/mrbysco/horsingaround/network/message/SyncDataMessage.java
@@ -2,15 +2,15 @@ package com.mrbysco.horsingaround.network.message;
 
 import com.mrbysco.horsingaround.client.ClientHandler;
 import com.mrbysco.horsingaround.data.CallData;
-import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.DistExecutor.SafeRunnable;
 import net.minecraftforge.network.NetworkEvent.Context;
 
+import java.io.Serial;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -37,37 +37,51 @@ public class SyncDataMessage {
 	public void handle(Supplier<Context> context) {
 		Context ctx = context.get();
 		ctx.enqueueWork(() -> {
-			if (ctx.getDirection().getReceptionSide().isClient() && FMLEnvironment.dist.isClient()) {
-				Player player = Minecraft.getInstance().player;
-				if (player != null && player.getUUID().equals(this.playerUUID)) {
-					ClientHandler.tamedList.clear();
-
-					ListTag dataListTag = new ListTag();
-					String uuid = playerUUID.toString();
-					if (data.getTagType(uuid) == 9) {
-						Tag nbt = data.get(uuid);
-						if (nbt instanceof ListTag listNBT) {
-							if (!listNBT.isEmpty() && listNBT.getElementType() != CompoundTag.TAG_COMPOUND) {
-								return;
-							}
-
-							dataListTag = listNBT;
-						}
-					}
-					if (!dataListTag.isEmpty()) {
-						List<CallData.TamedData> dataList = new ArrayList<>();
-						for (int i = 0; i < dataListTag.size(); ++i) {
-							CompoundTag dataTag = dataListTag.getCompound(i);
-							CallData.TamedData data = CallData.TamedData.load(dataTag);
-							if (data != null) {
-								dataList.add(data);
-							}
-						}
-						ClientHandler.tamedList.addAll(dataList);
-					}
-				}
+			if (ctx.getDirection().getReceptionSide().isClient()) {
+				SyncData.update(this.playerUUID, this.data).run();
 			}
 		});
 		ctx.setPacketHandled(true);
+	}
+
+	private static class SyncData {
+		private static SafeRunnable update(UUID playerUUID, CompoundTag data) {
+			return new DistExecutor.SafeRunnable() {
+				@Serial
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public void run() {
+					net.minecraft.world.entity.player.Player player = net.minecraft.client.Minecraft.getInstance().player;
+					if (player != null && player.getUUID().equals(playerUUID)) {
+						ClientHandler.tamedList.clear();
+
+						ListTag dataListTag = new ListTag();
+						String uuid = playerUUID.toString();
+						if (data.getTagType(uuid) == 9) {
+							Tag nbt = data.get(uuid);
+							if (nbt instanceof ListTag listNBT) {
+								if (!listNBT.isEmpty() && listNBT.getElementType() != CompoundTag.TAG_COMPOUND) {
+									return;
+								}
+
+								dataListTag = listNBT;
+							}
+						}
+						if (!dataListTag.isEmpty()) {
+							List<CallData.TamedData> dataList = new ArrayList<>();
+							for (int i = 0; i < dataListTag.size(); ++i) {
+								CompoundTag dataTag = dataListTag.getCompound(i);
+								CallData.TamedData data = CallData.TamedData.load(dataTag);
+								if (data != null) {
+									dataList.add(data);
+								}
+							}
+							ClientHandler.tamedList.addAll(dataList);
+						}
+					}
+				}
+			};
+		}
 	}
 }
