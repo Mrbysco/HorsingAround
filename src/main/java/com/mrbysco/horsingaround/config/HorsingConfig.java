42d9d74540e44c9d68ac89b6ee7c1ae6f3e88a4d
Mrbysco <Mrbysco@users.noreply.github.com>
2026-06-14 13:57:11 +0200

    Add slotsVisible client option
    To configure how many tamed entities can show per page

--- src/main/java/com/mrbysco/horsingaround/config/HorsingConfig.java
+++ src/main/java/com/mrbysco/horsingaround/config/HorsingConfig.java
@@ -7,6 +7,41 @@ import net.neoforged.fml.event.config.ModConfigEvent;
 import org.apache.commons.lang3.tuple.Pair;
 
 public class HorsingConfig {
+
+	public static class Client {
+		public final ModConfigSpec.BooleanValue pixelatedMode;
+		public final ModConfigSpec.IntValue hoverColor;
+		public final ModConfigSpec.IntValue slotsVisible;
+
+		Client(ModConfigSpec.Builder builder) {
+			builder.comment("Client settings")
+					.push("Client");
+
+			pixelatedMode = builder
+					.comment("Use hexagonal style for the radial menu slices instead of smooth circle")
+					.define("pixelatedMode", false);
+
+			hoverColor = builder
+					.comment("The integer color of the radial menu when hovering a slice [Default: 4170175]")
+					.defineInRange("hoverColor", 0x3FA1BF, 0x000000, 0xFFFFFF);
+
+			slotsVisible = builder
+					.comment("The max number of tamed entities visible in the radial menu [Default: 20]")
+					.defineInRange("slotsVisible", 20, 1, 100);
+
+			builder.pop();
+		}
+	}
+
+	public static final ModConfigSpec clientSpec;
+	public static final Client CLIENT;
+
+	static {
+		final Pair<Client, ModConfigSpec> specPair = new ModConfigSpec.Builder().configure(Client::new);
+		clientSpec = specPair.getRight();
+		CLIENT = specPair.getLeft();
+	}
+
 	public static class Common {
 		public final ModConfigSpec.BooleanValue addOnMount;
 		public final ModConfigSpec.BooleanValue addOnTame;
