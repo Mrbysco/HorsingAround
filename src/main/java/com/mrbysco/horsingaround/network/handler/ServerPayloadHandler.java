6ef4f005af26dca2211ef1f4cf19bd00bf9848b4
Mrbysco <Mrbysco@users.noreply.github.com>
2026-08-01 21:10:46 +0200

    Adjust animal calling behavior
    Check for the entity in multiple dimensions before assuming it's not found

--- src/main/java/com/mrbysco/horsingaround/network/handler/ServerPayloadHandler.java
+++ src/main/java/com/mrbysco/horsingaround/network/handler/ServerPayloadHandler.java
@@ -3,16 +3,11 @@ package com.mrbysco.horsingaround.network.handler;
 import com.mrbysco.horsingaround.data.CallData;
 import com.mrbysco.horsingaround.network.message.SummonPayload;
 import com.mrbysco.horsingaround.network.message.UnlinkPayload;
-import com.mrbysco.horsingaround.registry.HorsingRegistry;
+import com.mrbysco.horsingaround.util.CallUtil;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
-
-import java.util.List;
-import java.util.UUID;
 
 public class ServerPayloadHandler {
 	public static final ServerPayloadHandler INSTANCE = new ServerPayloadHandler();
@@ -25,34 +20,7 @@ public class ServerPayloadHandler {
 		context.enqueueWork(() -> {
 					Player player = context.player();
 					if (player instanceof ServerPlayer serverPlayer) {
-						ServerLevel level = serverPlayer.level();
-						UUID mobUUID = summonData.mobUUID();
-
-						Entity mob = level.getEntity(mobUUID);
-						CallData callData = CallData.get(serverPlayer.level());
-						if (mob == null) {
-							List<CallData.TamedData> tamedList = callData.getTamedData(serverPlayer.getUUID());
-							CallData.TamedData matchingData = null;
-							for (CallData.TamedData data : tamedList) {
-								if (data.uuid().equals(mobUUID)) {
-									matchingData = data;
-									break;
-								}
-							}
-
-							if (matchingData != null) {
-								Entity entity = matchingData.createEntity(serverPlayer.level());
-								entity.setPos(serverPlayer.position());
-								level.addFreshEntity(entity);
-							}
-						} else {
-							mob.teleportTo(serverPlayer.getX(), serverPlayer.getY(), serverPlayer.getZ());
-							callData.updateData(mob.getUUID(), mob);
-						}
-						callData.syncData(serverPlayer.getUUID());
-
-						level.playSound((Player) null, serverPlayer.blockPosition(), HorsingRegistry.CALL.get(), serverPlayer.getSoundSource(),
-								1.0F, 1.0F + (level.getRandom().nextFloat() - level.getRandom().nextFloat()) * 0.4F);
+						CallUtil.callAnimal(serverPlayer, summonData.mobUUID());
 					}
 				})
 				.exceptionally(e -> {
